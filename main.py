54e36edfeacd6d8a8a17d69279f226f877b4d38e
Klymok Nazariy <klymok.nazariy@gmail.com>
2020-12-28 23:42:37 +0200

    added responsing with geolocation, distance and time to get to target

--- main.py
+++ main.py
@@ -3,15 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import logging
+
+import requests
 import telegram
 import smtplib
+import objectpath
 from telegram.ext import Updater, Handler, MessageHandler
 from telegram.ext import Filters
 from telegram.ext import CommandHandler
 
 # API keys
 api_file = open('api_key.txt', 'r')
-gmaps_token = api_file.readline()
+ors_token = api_file.readline()
 tg_token = api_file.readline()
 api_file.close()
 
@@ -26,22 +29,50 @@ location_keyboard = telegram.KeyboardButton(text="Знайти паркінг п
 custom_keyboard = [[ location_keyboard ]]
 reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
 
-# User data
 current_location = [None] * 2
-
+longitude = 0
+latitude = 0
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Цей бот допоможе знайти вам найближчий паркінг!")
     bot.send_message(chat_id=update.effective_chat.id,
-                     reply_markup=reply_markup)
+                     reply_markup=reply_markup, text="Будь ласка, відправте ваші геодані.")
+
+
+def get_summary(json):
+    return json["features"][0]["properties"]["summary"]
 
 
 def get_location(update, context):
+    global longitude
+    global latitude
     longitude = update.message.location.longitude
     latitude = update.message.location.latitude
     current_location[0] = longitude
     current_location[1] = latitude
-    update.message.reply_text(text=current_location)
+    respond_nearest_parking(update, context)
+
+
+def respond_nearest_parking(update, context):
+    global longitude
+    global latitude
+    end_longitude = 24.0266211
+    end_latitude = 49.8433737
+    request = 'https://api.openrouteservice.org/v2/directions/driving-car?api_key=' + ors_token + \
+            '&start=' + str(longitude) + ',' + str(latitude) + \
+              '&end=' + str(end_longitude) + ',' + str(end_latitude)
+    response = requests.get(request)
+    if response.status_code == 200:
+        summary = get_summary(response.json())
+        distance = round(summary["distance"] / 1000, 2)
+        duration = round(summary["duration"] / 60)
+        print(str(distance) + ' км ' + str(duration) + ' хв')
+        update.message.reply_location(longitude=end_longitude, latitude=end_latitude)
+        update.message.reply_text(
+            text='Найближчий паркінг: ' + str(distance).format() + ' км\n'
+                'Орієнтовне прибуття: через ' + str(duration) + ' хв')
+    else:
+        update.message.reply_text(text='Вибачте, неможливо отримати інформацію про паркінг.')
 
 
 # Press the green button in the gutter to run the script.
