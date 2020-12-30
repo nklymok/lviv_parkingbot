af6e56344b34d7accaa353a294f44c994ed13762
Klymok Nazariy <klymok.nazariy@gmail.com>
2020-12-30 23:46:46 +0200

    added ParkingSpot class, searching for the closest parking, optimal size for reply markup

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import logging
+from math import radians, cos, sin, asin, sqrt
+from pathlib import Path
 
+import openpyxl
 import requests
 import telegram
-import openpyxl
-from pathlib import Path
-from math import radians, cos, sin, asin, sqrt
-from telegram.ext import Updater, Handler, MessageHandler
-from telegram.ext import Filters
 from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import Updater, MessageHandler
+
+from parkingspot import ParkingSpot
 
 # API keys
 api_file = open('api_key.txt', 'r')
@@ -28,16 +30,16 @@ dispatcher = updater.dispatcher
 
 # location keyboard
 location_keyboard = telegram.KeyboardButton(text="Знайти паркінг поруч", request_location=True)
-custom_keyboard = [[ location_keyboard ]]
-reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+custom_keyboard = [[location_keyboard]]
+reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
 
 # excel sheet
 xlsx_file = Path('parking.xlsx')
 workbook_obj = openpyxl.load_workbook(xlsx_file)
 parking_sheet = workbook_obj.active
 
-# parking dict
-parking_data = {}
+# parking list
+parking_spots = []
 
 # User data
 current_location = [None] * 2
@@ -61,15 +63,7 @@ def haversine(lon1, lat1, lon2, lat2):
     return c * r
 
 
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Цей бот допоможе знайти вам найближчий паркінг!")
-    bot.send_message(chat_id=update.effective_chat.id,
-                     reply_markup=reply_markup, text="Будь ласка, відправте ваші геодані.")
-
-
-def get_summary(json):
+def get_dist_dur_summary(json):
     return json["features"][0]["properties"]["summary"]
 
 
@@ -83,34 +77,66 @@ def get_location(update, context):
     respond_nearest_parking(update, context)
 
 
+def find_distance(parking_spot):
+    global longitude
+    global latitude
+
+    return haversine(longitude, latitude, parking_spot.longitude, parking_spot.latitude)
+
+
+def get_nearest_parkingspot():
+    closest = parking_spots[0]
+    min_distance = find_distance(closest)
+    for parking in parking_spots:
+        if find_distance(parking) < min_distance:
+            closest = parking
+            min_distance = find_distance(parking)
+    return closest
+
+
 def respond_nearest_parking(update, context):
     global longitude
     global latitude
-    end_longitude = 24.0266211
-    end_latitude = 49.8433737
+
+    parking_spot = get_nearest_parkingspot()
     request = 'https://api.openrouteservice.org/v2/directions/driving-car?api_key=' + ors_token + \
             '&start=' + str(longitude) + ',' + str(latitude) + \
-              '&end=' + str(end_longitude) + ',' + str(end_latitude)
+              '&end=' + str(parking_spot.longitude) + ',' + str(parking_spot.latitude)
     response = requests.get(request)
     if response.status_code == 200:
-        summary = get_summary(response.json())
+        summary = get_dist_dur_summary(response.json())
         distance = round(summary["distance"] / 1000, 2)
         duration = round(summary["duration"] / 60)
         print(str(distance) + ' км ' + str(duration) + ' хв')
-        update.message.reply_location(longitude=end_longitude, latitude=end_latitude)
+        update.message.reply_location(latitude=parking_spot.latitude, longitude=parking_spot.longitude)
         update.message.reply_text(
-            text='Найближчий паркінг: ' + str(distance).format() + ' км\n'
-                'Орієнтовне прибуття: через ' + str(duration) + ' хв')
+            text='🚗 Найближчий паркінг: ' + parking_spot.address + '\n\n'
+                '📏 Відстань: ' + str(distance).format() + ' км\n\n'
+                '⌛ Орієнтовне прибуття: через ' + str(duration) + ' хв')
     else:
         update.message.reply_text(text='Вибачте, неможливо отримати інформацію про паркінг.')
+        print(response.json())
 
 
 def load_parking_data():
-    to_be_continued = None
+    for i, row in enumerate(parking_sheet.iter_rows(values_only=True)):
+        # row[8] = longitude,latitude | row[10] == address
+        if i == 0 or row[1] != 'загального користування':
+            continue
+        address = str(row[10])
+        lat_lon = str(row[8]).split(',', 2)
+        parking_spots.append(ParkingSpot(address, float(lat_lon[0]), float(lat_lon[1])))
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Цей бот допоможе знайти вам найближчий паркінг!")
+    bot.send_message(chat_id=update.effective_chat.id,
+                     reply_markup=reply_markup, text="Будь ласка, відправте ваші геодані.")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    load_parking_data()
     start_handler = CommandHandler('start', start)
     location_handler = CommandHandler('send_location', get_location)
     dispatcher.add_handler(start_handler)
