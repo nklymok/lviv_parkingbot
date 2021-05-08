e6f44cee1edf49bde464aa5c351f4d771577ef00
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-05-08 16:31:57 +0300

    changed webhook url mapping

--- main.py
+++ main.py
@@ -254,10 +254,10 @@ def main():
     dispatcher.add_handler(MessageHandler(filters=Filters.location, callback=process_location))
     dispatcher.add_handler(CallbackQueryHandler(next_parking_lot))
     # updater.start_polling()
-    updater.start_webhook(listen="123.123.12.21",
+    updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=tg_token)
-    updater.bot.setWebhook('https://parkingbot-lviv.herokuapp.com/' + tg_token)
+                          url_path=tg_token,
+                          webhook_url='https://parkingbot-lviv.herokuapp.com/' + tg_token)
     updater.idle()
 
 
